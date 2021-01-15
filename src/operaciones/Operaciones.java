e50e71c0b7ef94ac28d743c94d857167d7ff1c67
sebastian beltran zapata <sebastian199502@hotmail.com>
2021-01-15 14:03:25 -0500

    Ejercicio 5

---
+++ src/operaciones/Operaciones.java
@@ -0,0 +1,26 @@
+package operaciones;
+
+/**
+ *
+ * @author sebas
+ */
+import java.util.Scanner;
+public class Operaciones {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+
+        int i = 1;
+        
+        while (i <= 100){ 
+        System.out.println(i);
+        i++;
+        }
+
+        
+
+
+    }
+}
